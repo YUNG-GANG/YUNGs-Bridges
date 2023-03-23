434a43a81e0008d74ba2fc45f05169b568fbaba4
Nick Young <yungnickyoung@gmail.com>
2023-03-23 02:24:07 -0400

    1.19.4 porting progress.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungsbridges/YungsBridgesCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsbridges/YungsBridgesCommon.java
@@ -1,7 +1,6 @@
 package com.yungnickyoung.minecraft.yungsbridges;
 
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
-import com.yungnickyoung.minecraft.yungsbridges.module.ConfigModule;
 import com.yungnickyoung.minecraft.yungsbridges.services.Services;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,7 +8,6 @@ import org.apache.logging.log4j.Logger;
 public class YungsBridgesCommon {
     public static final String MOD_ID = "yungsbridges";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-    public static final ConfigModule CONFIG = new ConfigModule();
 
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.yungsbridges.module");
--- Common/src/main/java/com/yungnickyoung/minecraft/yungsbridges/module/ConfigModule.java
+++
@@ -1,5 +0,0 @@
-package com.yungnickyoung.minecraft.yungsbridges.module;
-
-public class ConfigModule {
-    // Config has been moved to data pack JSON as of 1.19
-}
