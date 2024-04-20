67be028ecb170faf881c1b1df22ece655b3cf11f
drullkus <drullkus@gmail.com>
2024-04-20 12:29:16 -0700

    Create NeoForge subproject

---
+++ NeoForge/src/main/java/com/yungnickyoung/minecraft/yungsbridges/YungsBridgesNeoForge.java
@@ -0,0 +1,10 @@
+package com.yungnickyoung.minecraft.yungsbridges;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(YungsBridgesCommon.MOD_ID)
+public class YungsBridgesNeoForge {
+    public YungsBridgesNeoForge() {
+        YungsBridgesCommon.init();
+    }
+}
---
+++ NeoForge/src/main/java/com/yungnickyoung/minecraft/yungsbridges/services/NeoForgeModulesLoader.java
@@ -0,0 +1,8 @@
+package com.yungnickyoung.minecraft.yungsbridges.services;
+
+public class NeoForgeModulesLoader implements IModulesLoader {
+    @Override
+    public void loadModules() {
+        IModulesLoader.super.loadModules(); // Load common modules
+    }
+}
