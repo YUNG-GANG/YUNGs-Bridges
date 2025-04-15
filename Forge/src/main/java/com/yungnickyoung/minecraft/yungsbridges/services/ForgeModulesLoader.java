70ec4a9a45d4999f5e818d95f72bc2f1408fcbe6
Nick Young <yungnickyoung@gmail.com>
2025-04-14 20:59:21 -0400

    v5.4.0. Port to 1.21.4.

--- Forge/src/main/java/com/yungnickyoung/minecraft/yungsbridges/services/ForgeModulesLoader.java
+++
@@ -1,8 +0,0 @@
-package com.yungnickyoung.minecraft.yungsbridges.services;
-
-public class ForgeModulesLoader implements IModulesLoader {
-    @Override
-    public void loadModules() {
-        IModulesLoader.super.loadModules(); // Load common modules
-    }
-}
